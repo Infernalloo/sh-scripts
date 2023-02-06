f94ae7bfe0380c91619664c5b05d8915038c6453
inferno <inferno@pop-os.localdomain>
2023-02-06 21:24:37 +0100

    deleted dotlife clone form other.sh

--- Mint/other.sh
+++ Mint/other.sh
@@ -6,8 +6,3 @@ sudo apt install fish -y
 # install starship prompt
 curl -sS https://starship.rs/install.sh | sh
 
-# clone dotfiles form github 
-cd Documents
-mkdir github
-cd github
-git clone https://github.com/Infernalloo/dotfiles
