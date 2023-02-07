97b84a81193204369adbf784e3fb1cbb9fe8b7fa
inferno <inferno@pop-os.localdomain>
2023-02-07 20:16:14 +0100

    added -y to all install commands

--- Mint/browsers.sh
+++ Mint/browsers.sh
@@ -2,27 +2,27 @@
 
 ## Browsers ##
 #  Vivaldi
-sudo apt install wget
+sudo apt -y install wget
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
 echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
-sudo apt update && sudo apt install vivaldi-stable
+sudo apt update -y && sudo apt install vivaldi-stable -y
 
 # Brave
-sudo apt install apt-transport-https curl
+sudo apt install -y apt-transport-https curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser
+sudo apt update -y
+sudo apt install brave-browser -y
 
 # Opera
-sudo apt install curl
+sudo apt install curl -y
 curl https://deb.opera.com/archive.key | sudo apt-key add -
 echo deb https://deb.opera.com/opera-stable/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list
-sudo apt update
-sudo apt install opera-stable
+sudo apt update -y
+sudo apt install opera-stable -y
 
 # Librewolf
-sudo apt update && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
+sudo apt update -y && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
 distro=$(if echo " una vanessa focal jammy bullseye vera uma" | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
 wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
 sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
@@ -33,5 +33,5 @@ Components: main
 Architectures: amd64
 Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
-sudo apt update
+sudo apt update -y
 sudo apt install librewolf -y
