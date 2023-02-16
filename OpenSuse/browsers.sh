08e2dad95585419786846117ae63328af09a21e2
Infernalloo <infernodev00@gmail.com>
2023-02-16 10:48:26 +0100

    browsers.sh

--- OpenSuse/browsers.sh
+++ OpenSuse/browsers.sh
@@ -9,3 +9,12 @@ sudo zypper -n install curl
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
 sudo zypper -n install brave-browser
+
+# install opera
+sudo zypper ref
+sudo zypper -n install opera
+# install edge
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo zypper ar https://packages.microsoft.com/yumrepos/edge microsoft-edge-beta
+sudo zypper refresh
+sudo zypper -n install microsoft-edge-beta
