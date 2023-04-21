410c277a3ffa7ca54f0eb134c8251cfa6db82f2b
Infernalloo <infernodev00@gmail.com>
2023-04-21 23:06:40 +0200

    changed cli.sh in Fedora folder

--- Fedora/cli.sh
+++ Fedora/cli.sh
@@ -13,5 +13,13 @@ wget https://raw.githubusercontent.com/unxsh/nitch/main/setup.sh && sh setup.sh
 cd -
 
 # install bunnyfetch
-sudo dnf install go -y
-go install github.com/Rosettea/bunnyfetch@latest
+git clone https://github.com/Rosettea/Bunnyfetch
+cd Bunnyfetch
+go get -d ./...
+go build -ldflags "-w -s" # ldflags make the binary smaller
+
+# install pfetch
+wget https://github.com/dylanaraps/pfetch/archive/master.zip
+unzip master.zip
+sudo install pfetch-master/pfetch /usr/local/bin/
+ls -l /usr/local/bin/pfetch
