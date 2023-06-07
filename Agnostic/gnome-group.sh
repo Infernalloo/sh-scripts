63288029f1c93fdc3644fa1684d49e8e78aee24d
Infernalloo <infernodev00@gmail.com>
2023-06-07 10:56:58 +0200

    added flathub install script

---
+++ Agnostic/gnome-group.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+## Games
+flatpak install flathub org.gnome.Chess -y
+flatpak install flathub org.gnome.Mahjongg -y
+flatpak install flathub org.gnome.Sudoku -y
+flatpak install flathub org.gnome.Mines -y
+flatpak install flathub org.gnome.TwentyFortyEight -y
+flatpak install flathub org.gnome.Quadrapassel -y
+flatpak install flathub org.gnome.Reversi -y
+flatpak install flathub org.gnome.Tetravex -y
+flatpak install flathub org.gnome.Nibbles -y
+flatpak install flathub org.gnome.Tali -y
+flatpak install flathub org.gnome.Robots -y
+flatpak install flathub org.gnome.Taquin -y
+flatpak install flathub org.gnome.Four-in-a-row -y
+flatpak install flathub org.gnome.five-or-more -y
+flatpak install flathub org.gnome.LightsOff -y
+flatpak install flathub org.gnome.Klotski -y
+flatpak install flathub org.gnome.SwellFoop -y
+flatpak install flathub org.gnome.Aisleriot -y
+flatpak install flathub org.gnome.Sudoku -y
+
+## Apps
+flatpak install flathub org.gnome.gitg -y
+flatpak install flathub org.gnome.Boxes -y
+flatpak install flathub org.gnome.Extensions -y
+flatpak install flathub org.gnome.Calculator -y
+flatpak install flathub org.gnome.clocks -y
+flatpak install flathub org.gnome.Maps -y
+flatpak install flathub org.gnome.TextEditor -y
+flatpak install flathub org.gnome.baobab -y
+flatpak install flathub org.gnome.Snapshot -y
+flatpak install flathub org.gnome.SoundRecorder -y
+flatpak install flathub org.gnome.Calendar -y
+
+## Group project
+flatpak install flathub org.gnome.Totem -y
+flatpak install flathub com.raggesilver.BlackBox -y
+flatpak install flathub org.gnome.Characters -y
+flatpak install flathub org.gnome.Evince -y
+flatpak install flathub com.belmoussaoui.Obfuscate -y
+flatpak install flathub org.gnome.Todo -y
+flatpak install flathub org.gnome.gitlab.somas.Apostrophe -y
+flatpak install flathub org.gnome.Notes -y
+flatpak install flathub org.gnome.design.Palette -y
