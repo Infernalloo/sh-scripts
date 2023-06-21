f85f764a1fa4fd9655642a008cb6a2fb66733538
Infernalloo <infernodev00@gmail.com>
2023-06-21 14:05:30 +0200

    modified browsers.sh in Deb folder

--- Deb/browsers.sh
+++ Deb/browsers.sh
@@ -2,19 +2,28 @@
 
 ## Browsers ##
 
+# Firefox
+sudo apt install -y firefox
+
+# Vivaldi
+wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
+echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
+sudo apt update -y && sudo apt install vivaldi-stable -y
+
 # Brave
-sudo apt install -y apt-transport-https curl
+sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update -y
-sudo apt install brave-browser -y
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
 
 # Opera
-sudo apt install curl -y
-curl https://deb.opera.com/archive.key | sudo apt-key add -
-echo deb https://deb.opera.com/opera-stable/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list
-sudo apt update -y
-sudo apt install opera-stable -y
+sudo apt install software-properties-common apt-transport-https wget ca-certificates gnupg2
+wget -O- https://deb.opera.com/archive.key | sudo gpg --dearmor | sudo tee /usr/share/keyrings/opera.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/opera.gpg] https://deb.opera.com/opera-stable/ stable non-free | sudo tee /etc/apt/sources.list.d/opera.list
+sudo apt update
+sudo apt install opera-stable
 
 # Librewolf
 # sudo apt update -y && sudo apt install -y wget gnupg lsb-release apt-transport-https ca-certificates
--- Deb/newdeb.sh
+++ Deb/newdeb.sh
@@ -39,7 +39,6 @@ sudo apt update
 sudo apt install
 
 #  Vivaldi
-sudo apt -y install wget
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | gpg --dearmor | sudo dd of=/usr/share/keyrings/vivaldi-browser.gpg
 echo "deb [signed-by=/usr/share/keyrings/vivaldi-browser.gpg arch=$(dpkg --print-architecture)] https://repo.vivaldi.com/archive/deb/ stable main" | sudo dd of=/etc/apt/sources.list.d/vivaldi-archive.list
 sudo apt update -y && sudo apt install vivaldi-stable -y
