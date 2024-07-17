b0c26475b94138038addbde25c5b63887c1de06b
Alex <infernalloo@proton.me>
2024-07-17 20:01:48 +0200

    Update pac.sh

--- pac/pac.sh
+++ pac/pac.sh
@@ -6,11 +6,7 @@ sudo pacman -Syu --noconfirm
 # base utilities
 sudo pacman -S base base-devel git wget curl gpg --noconfirm
 
-# Install aur helpers
-cd
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur
-makepkg -fsri
+# Install yay
 cd
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd
@@ -18,16 +14,14 @@ cd
 # Terminal related
 sudo pacman -S eza vim neovim alacritty --noconfirm
 sudo pacman -S neofetch bottom cmatrix asciiquarium lolcat --noconfirm
-pikaur -S bunnyfetch pfetch nitch --noconfirm
-pikaur -S ttf-jetbrains-mono-git --noconfirm
+yay -S bunnyfetch pfetch nitch --noconfirm
 
-# Browsers
-sudo pacman -S firefox --noconfirm
-sudo pacman -S vivaldi vivaldi-ffmpeg-codecs --noconfirm
-pikaur -S brave --noconfirm
+# Apps
+yay -S brave --noconfirm
+yay -S visual-studio-code-bin --noconfirm
+yay -S spotify --noconfirm
 
-# Others
-# steam
+## STEAM ##
 sudo pacman -S steam --noconfirm
 # wine dependencies
 sudo pacman -S --needed wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls \
@@ -38,11 +32,3 @@ ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-li
 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 # amd drivers
 sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
-# code
-pikaur -S visual-studio-code-bin --noconfirm
-
-# Flatpak
-sudo pacman -S flatpak --noconfirm
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.spotify.Client -y
-flatpak install flathub com.usebottles.bottles -y
